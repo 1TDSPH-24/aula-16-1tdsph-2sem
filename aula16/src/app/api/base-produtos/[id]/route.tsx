0ab38593d842be871fced75b934fb173972f24ef
dudatiemiak <rm554756@fiap.com.br>
2024-10-07 12:06:36 -0300

    Implementando request DELETE no routes.tsx

--- aula16/src/app/api/base-produtos/[id]/route.tsx
+++ aula16/src/app/api/base-produtos/[id]/route.tsx
@@ -11,3 +11,22 @@ export async function GET(request:Request,{params}:{params:{id:number}}){
 
     return NextResponse.json(produto);
 }
+
+export async function DELETE(request:Request,{params}:{params:{id:number}}){
+    //RECUPERANDO A LISTA PARA SER MANIPULADA
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
+    const produtos:TipoProduto[] = JSON.parse(file);
+
+    //ENCONTRANDO O ID DO OBJETO PARA SER REMOVIDO COM findIdex
+    const idProduto = produtos.findIndex( p => p.id == params.id );
+
+    //UTILIZAR O MÉTODO SPLICE PARA REMOVER ESTE OBJETO DA LISTA, UTILIZANDO O INDICE ENCONTRADO:
+    produtos.splice(idProduto,1);
+
+    //PRECISAMOS DEVOLVER AS LISTA PARA O ARQUIVO BASE.JSON()
+    const fileUpdate = JSON.stringify(produtos);
+    await fs.writeFile(process.cwd() + '/src/data/base.json', fileUpdate);
+
+
+    return NextResponse.json({msg: "Produto removido com sucesso!"});
+}
