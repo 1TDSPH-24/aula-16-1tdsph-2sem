086997c5a6ff64b6ef43f0da449d742554c74129
ovitortadue <souavitor6@gmail.com>
2024-10-07 12:06:35 -0300

    implemnetando request deletarararaarararaarara no routeeeeeeeee.stsx

--- aula16/src/app/api/base-produtos/[id]/route.tsx
+++ aula16/src/app/api/base-produtos/[id]/route.tsx
@@ -1,13 +1,29 @@
 import { TipoProduto } from "@/types";
-import { promises as fs} from "fs"
+import { promises as fs } from "fs";
 import { NextResponse } from "next/server";
 
-export async function GET(request:Request,{params}:{params:{id:number}}){
-    
-    const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
-    const produtos:TipoProduto[] = JSON.parse(file);
+export async function GET(request: Request, { params }: { params: { id: number } }) {
 
-    const produto = produtos.find( p => p.id == params.id );
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json', 'utf-8');
+    const produtos: TipoProduto[] = JSON.parse(file);
+
+    const produto = produtos.find(p => p.id == params.id);
 
     return NextResponse.json(produto);
+}
+export async function DELETE(request: Request, { params }: { params: { id: number } }) {
+
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json', 'utf-8');
+    const produtos: TipoProduto[] = JSON.parse(file);
+
+    const IdProduto = produtos.findIndex(p => p.id == params.id);
+
+    produtos.splice(IdProduto, 1);
+
+    //1 - Convertendo a lista em STRING
+    const fileUpdate = JSON.stringify(produtos);
+    //2 - Escrevendo no arquivo através do caminho especificado.
+    await fs.writeFile(process.cwd() + '/src/data/base.json', fileUpdate);
+
+    return NextResponse.json({ msg: "Produto removido c sucesors" });
 }
