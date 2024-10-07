67e90be7e75545a3dd07e22d09ddb0e357d8a648
gustavodscruz <gustavodiasdsc@gmail.com>
2024-10-07 11:12:16 -0300

    criacao de fallback

---
+++ aula16/src/app/loading.tsx
@@ -0,0 +1,16 @@
+import Spinner from '@/components/Spinner/Spinner'
+import React from 'react'
+
+const Loading = () => {
+    console.log('omnkmndskmdaslkmk')
+  return (
+    <div>
+      {/* <h1>Loading...</h1> */}
+      <Spinner>
+
+      </Spinner>
+    </div>
+  )
+}
+
+export default Loading
