9faebc2a1309ef41202c791eb1af9aa642c6a9db
ovitortadue <souavitor6@gmail.com>
2024-10-07 11:14:50 -0300

    criacao de fallback loading (cair pra trás)

---
+++ aula16/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+
+export default function Spinner() {
+    return (
+        <div className="flex justify-center items-center">
+            <div className="anime-spin rounded-full h-16 w-16 
+            border-t-4 border-blue-500"> </div>
+        </div>
+    )
+}
