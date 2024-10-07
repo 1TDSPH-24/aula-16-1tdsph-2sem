0b46de9d1090cba17901825c8d9162031e4d3c49
felipepizzinato <felipepizzinato9@gmail.com>
2024-10-07 12:06:17 -0300

    finalizando aula

--- aula16/src/app/layout.tsx
+++ aula16/src/app/layout.tsx
@@ -10,6 +10,8 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
+ 
+ 
   return (
     <html lang="pt-br">
       <body>
