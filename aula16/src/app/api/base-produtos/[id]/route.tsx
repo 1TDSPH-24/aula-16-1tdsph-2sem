03ffc9f97186363bffdb72af5064ec5ee3c024a1
Victor-Estrella <victorhcarracci@gmail.com>
2024-10-07 12:07:03 -0300

    Implementando request DELETE no routes.tsx

--- aula16/src/app/api/base-produtos/[id]/route.tsx
+++ aula16/src/app/api/base-produtos/[id]/route.tsx
@@ -11,3 +11,20 @@ export async function GET(request:Request,{params}:{params:{id:number}}){
 
     return NextResponse.json(produto);
 }
+
+export async function DELETE(request:Request,{params}:{params:{id:number}}){
+    
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
+    const produtos:TipoProduto[] = JSON.parse(file);
+
+    const idProduto = produtos.findIndex( p => p.id == params.id );
+
+    produtos.splice(idProduto,1);
+
+    //1 - Convertendo a lista em STRING
+    const fileUpdate = JSON.stringify(produtos);
+    //2 - Escrevendo no arquivo através do caminho especificado.
+    await fs.writeFile(process.cwd() + '/src/data/base.json', fileUpdate);
+
+    return NextResponse.json({msg:"Produto Removido com sucesso!"});
+}
